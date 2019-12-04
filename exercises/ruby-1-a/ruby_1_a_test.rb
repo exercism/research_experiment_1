6bb4d6bbc791908f0e8f013a99aa8e2f660ddddd
Jeremy Walker <jez.walker@gmail.com>
2019-12-04 14:30:07 +0900

    Update stubbed Ruby tests

--- exercises/ruby-1-a/ruby_1_a_test.rb
+++ exercises/ruby-1-a/ruby_1_a_test.rb
@@ -1,1 +1,9 @@
-TODO
+require 'minitest/autorun'
+require_relative 'ruby_1_a'
+
+# Common test data version: 1.2.0 4fc1acb
+class StubbedTest < Minitest::Test
+  def test_passes
+    assert true
+  end
+end
