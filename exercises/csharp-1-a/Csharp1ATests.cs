72ad3495303cfc935b3c48ff0bc856216a394b1b
Erik Schierboom <erik_schierboom@hotmail.com>
2019-12-04 10:29:05 +0100

    csharp/a: add more tests

--- exercises/csharp-1-a/Csharp1ATests.cs
+++ exercises/csharp-1-a/Csharp1ATests.cs
@@ -5,9 +5,14 @@ public class SentenceTest
     public static TheoryData<string, string> SentenceAndWordWithMostVowels = new TheoryData<string, string>
     {
         { "at", "at" },
+        { "air", "air" },
+        { "lungs air", "air" },
+        { "breathe lungs air", "breathe" },
         { "at the races", "races" },
         { "Where will you go to tomorrow", "tomorrow" },
-        { "Circle Area ", "Area" }
+        { "Circle Area", "Area" },
+        { "Test Your Might", "Your" },
+        { "Extremely fresh vegetables", "vegetables" }
     };
 
     [Theory]
