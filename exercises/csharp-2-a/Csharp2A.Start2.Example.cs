bf75486f11c7abfde9cfec99db3af93b90a0fc27
Erik Schierboom <erik_schierboom@hotmail.com>
2019-12-04 14:10:59 +0100

    csharp/a: add part 2 (#9)
    csharp/a: add part 2

---
+++ exercises/csharp-2-a/Csharp2A.Start2.Example.cs
@@ -0,0 +1,44 @@
+using System;
+
+public static class Sentence
+{
+    public static string WordWithMostVowels(string sentence)
+    {
+        var words = sentence.Split(' ');
+        string wordWithMostVowels = null;
+        var mostVowelsPerWord = -1;
+        var mostConsecutiveConsonantsPerWord = -1;
+
+        foreach (var word in words)
+        {
+            var vowels = 0;
+            var maxConsecutiveConsonants = 0;
+            var currentConsecutiveConsonants = 0;
+
+            foreach (var character in word)
+            {
+                var lowerCharacter = char.ToLower(character);
+                if (lowerCharacter == 'a' || lowerCharacter == 'e' || lowerCharacter == 'i' || lowerCharacter == 'o' || lowerCharacter == 'u')
+                {
+                    vowels++;
+                    currentConsecutiveConsonants = 0;
+                }
+                else
+                {
+                    currentConsecutiveConsonants++;
+                    maxConsecutiveConsonants = Math.Max(maxConsecutiveConsonants, currentConsecutiveConsonants);
+                }
+            }
+
+            if (vowels > mostVowelsPerWord ||
+                vowels == mostVowelsPerWord && maxConsecutiveConsonants > mostConsecutiveConsonantsPerWord)
+            {
+                mostVowelsPerWord = vowels;
+                wordWithMostVowels = word;
+                mostConsecutiveConsonantsPerWord = maxConsecutiveConsonants;
+            }
+        }
+
+        return wordWithMostVowels;
+    }
+}
--- exercises/csharp-2-a/Csharp2ATests.cs
+++ exercises/csharp-2-a/Csharp2ATests.cs
@@ -1,1 +1,27 @@
-// TODO: implement
+using Xunit;
+
+public class LetterCountTest
+{
+    public static TheoryData<string, string> SentenceAndWordWithMostVowels = new TheoryData<string, string>
+    {
+        { "at", "at" },
+        { "air", "air" },
+        { "lungs air", "air" },
+        { "breathe lungs air", "breathe" },
+        { "at the races", "races" },
+        { "plain bear", "plain" },
+        { "Reeling noses hurts", "Reeling" },
+        { "Where will you go to tomorrow", "tomorrow" },
+        { "Circle Area ", "Area" },
+        { "evening Aeon multiple", "evening" },
+        { "amoebae denim", "amoebae" },
+        { "her old dad", "old" },
+        { "abide Moiety apace", "Moiety" },
+        { "Quaver Queer Moiety Quite", "Moiety" },
+    };
+
+    [Theory]
+    [MemberData(nameof(SentenceAndWordWithMostVowels))]
+    public void WordWithMostVowels(string sentence, string expected) =>
+        Assert.Equal(expected, Sentence.WordWithMostVowels(sentence));
+}
