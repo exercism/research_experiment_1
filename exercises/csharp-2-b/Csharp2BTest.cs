703f4c53f33fabc224b5bda8fd3ae40f3c179342
Erik Schierboom <erik_schierboom@hotmail.com>
2019-12-05 09:16:39 +0100

    csharp/b: add part 2

--- exercises/csharp-2-b/Csharp2BTest.cs
+++ exercises/csharp-2-b/Csharp2BTest.cs
@@ -1,1 +1,60 @@
-TODO
+using Xunit;
+
+public class StoreTest
+{
+    [Fact]
+    public void OnlyEmptyQueueIsQueue1() =>
+        Assert.Equal(1, Store.QueueWithMinimalWaitingTime(customersInQueue1: 0, customersInQueue2: 1, customersInQueue3: 2));
+
+    [Fact]
+    public void OnlyEmptyQueueIsQueue2() =>
+        Assert.Equal(2, Store.QueueWithMinimalWaitingTime(customersInQueue1: 3, customersInQueue2: 0, customersInQueue3: 2));
+
+    [Fact]
+    public void OnlyEmptyQueueIsQueue3() =>
+        Assert.Equal(3, Store.QueueWithMinimalWaitingTime(customersInQueue1: 1, customersInQueue2: 1, customersInQueue3: 0));
+
+    [Fact]
+    public void AllQueuesHaveSameNumberOfCustomersInQueue() =>
+        Assert.Equal(3, Store.QueueWithMinimalWaitingTime(customersInQueue1: 5, customersInQueue2: 5, customersInQueue3: 5));
+
+    [Fact]
+    public void NonEmptyQueuesWithFastestQueueHasMinimalWaitingTime() =>
+        Assert.Equal(3, Store.QueueWithMinimalWaitingTime(customersInQueue1: 2, customersInQueue2: 5, customersInQueue3: 4));
+
+    [Fact]
+    public void NonEmptyQueuesWithMiddleQueueHasMinimalWaitingTime() =>
+        Assert.Equal(2, Store.QueueWithMinimalWaitingTime(customersInQueue1: 3, customersInQueue2: 1, customersInQueue3: 6));
+
+    [Fact]
+    public void NonEmptyQueuesWithSlowestQueueHasMinimalWaitingTime() =>
+        Assert.Equal(1, Store.QueueWithMinimalWaitingTime(customersInQueue1: 2, customersInQueue2: 6, customersInQueue3: 15));
+
+    [Fact]
+    public void EmptyQueuesDontRequireExtraTills() =>
+        Assert.Equal(0, Store.ExtraTillsToOpen(customersInQueue1: 0, customersInQueue2: 0, customersInQueue3: 0));
+    
+    [Fact]
+    public void OneQueueExactlyMatchingMaximumWaitingTimeDoesntRequireExtraTills() =>
+        Assert.Equal(0, Store.ExtraTillsToOpen(customersInQueue1: 2, customersInQueue2: 2, customersInQueue3: 2));
+
+    [Fact]
+    public void OneQueueExactlyMatchingMaximumWaitingTimeRequiresOneExtraTill() =>
+        Assert.Equal(1, Store.ExtraTillsToOpen(customersInQueue1: 5, customersInQueue2: 1, customersInQueue3: 5));
+    
+    [Fact]
+    public void OneQueueExactlyMatchingMaximumCapacityDoesntRequireExtraTills() =>
+        Assert.Equal(0, Store.ExtraTillsToOpen(customersInQueue1: 1, customersInQueue2: 1, customersInQueue3: 6));
+    
+    [Fact]
+    public void MultipleQueuesExceedMaximumCapacityRequiresOneExtraTill() =>
+        Assert.Equal(1, Store.ExtraTillsToOpen(customersInQueue1: 5, customersInQueue2: 7, customersInQueue3: 0));
+    
+    [Fact]
+    public void MultipleQueuesExceedMaximumCapacityRequireTwoExtraTills() =>
+        Assert.Equal(2, Store.ExtraTillsToOpen(customersInQueue1: 9, customersInQueue2: 7, customersInQueue3: 8));
+    
+    [Fact]
+    public void MultipleQueuesExceedMaximumWaitingTimeRequireTwoExtraTills() =>
+        Assert.Equal(1, Store.ExtraTillsToOpen(customersInQueue1: 5, customersInQueue2: 1, customersInQueue3: 5));
+}
--- exercises/csharp-2-b/README.md
+++ exercises/csharp-2-b/README.md
@@ -1,1 +1,36 @@
-TODO
+## Introduction
+
+This is Part 2 of our research into how people write C# differently. This exercise should take 10-15 minutes. There is no right or wrong way to approach it - just do what feels the most natural to you. Feel free to use books, Google or Stack Overflow, just like you would if you were programming normally, but please don't actively look for a solution to the problem as this will negatively affect the research.
+
+## Instructions
+
+Your task is to extend existing code with new functionality. The existing code deals with a customer standing in a store and wanting to pay. There are three available queues to join, depending on the number of customers in the queue and its processing speed. The three queues process customers at the following rate:
+
+- Queue 1: 1 customer per 45 seconds
+- Queue 2: 1 customer per 30 seconds
+- Queue 3: 1 customer per 12 seconds
+
+The code you'll be working with has a method named `Store.QueueWithMinimalWaitingTime`, which takes the number of customers in each queue and returns the number of the queue with the minimal waiting time.
+
+For example:
+
+```csharp
+Store.QueueWithMinimalWaitingTime(customersInQueue1: 2, customersInQueue2: 3, customersInQueue3: 5)
+// Returns: 3
+```
+
+To try and keep customers happy, the store has issued two new rules:
+
+- A single queue should never have more than 6 customers.
+- The expected waiting time for a single queue should never be more than two minutes.
+
+If a queue violates one of these rules (or both), one or more temporary tills should be opened. The store manager will ensure just enough customers are diverted to these new, temporary tills to have the queue match the above rules again. New tills will process 1 customer per 20 seconds.
+
+Your job is to define a method `Store.ExtraTillsToOpen`, which takes the number of customers in each queue and can return how many extra tills should be opened in order to satisfy the above two rules (this can be zero).
+
+For example:
+
+```csharp
+`Store.ExtraTillsToOpen (customersInQueue1: 5, customersInQueue2: 1, customersInQueue3: 2)`
+// Returns: 1
+```
