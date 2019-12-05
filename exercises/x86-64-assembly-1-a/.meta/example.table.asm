e20d23e5084d81f7ae56b14ed58e95c6e89c3c29
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-12-05 11:37:40 +0100

    x86-64-assembly/a: add part 1

---
+++ exercises/x86-64-assembly-1-a/.meta/example.table.asm
@@ -0,0 +1,54 @@
+section .data
+CHAR_UPPER: equ 0x0001
+CHAR_LOWER: equ 0x0002
+
+info_table:
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , 0         , 0         , 0
+db 0         , CHAR_UPPER, CHAR_UPPER, CHAR_UPPER
+db CHAR_UPPER, CHAR_UPPER, CHAR_UPPER, CHAR_UPPER
+db CHAR_UPPER, CHAR_UPPER, CHAR_UPPER, CHAR_UPPER
+db CHAR_UPPER, CHAR_UPPER, CHAR_UPPER, CHAR_UPPER
+db CHAR_UPPER, CHAR_UPPER, CHAR_UPPER, CHAR_UPPER
+db CHAR_UPPER, CHAR_UPPER, CHAR_UPPER, CHAR_UPPER
+db CHAR_UPPER, CHAR_UPPER, CHAR_UPPER, 0
+db 0         , 0         , 0         , 0
+db 0         , CHAR_LOWER, CHAR_LOWER, CHAR_LOWER
+db CHAR_LOWER, CHAR_LOWER, CHAR_LOWER, CHAR_LOWER
+db CHAR_LOWER, CHAR_LOWER, CHAR_LOWER, CHAR_LOWER
+db CHAR_LOWER, CHAR_LOWER, CHAR_LOWER, CHAR_LOWER
+db CHAR_LOWER, CHAR_LOWER, CHAR_LOWER, CHAR_LOWER
+db CHAR_LOWER, CHAR_LOWER, CHAR_LOWER, CHAR_LOWER
+db CHAR_LOWER, CHAR_LOWER, CHAR_LOWER, 0
+db 0         , 0         , 0         , 0
+
+section .text
+global str_isalpha
+str_isalpha:
+    mov eax, 1
+    lea rcx, [rel info_table]
+.loop_start:
+    movzx edx, byte [rdi]
+    test byte [rcx + rdx], CHAR_UPPER|CHAR_LOWER
+    je .return_false
+    add rdi, 1
+    cmp byte [rdi], 0
+    jne .loop_start
+    ret
+.return_false:
+    xor eax, eax
+    ret
---
+++ exercises/x86-64-assembly-1-a/README.md
@@ -0,0 +1,16 @@
+## Introduction
+
+This is Part 1 of our research into how people write x86-64 assembly differently. This exercise should take 10-15 minutes. There is no right or wrong way to approach it - just do what feels the most natural to you. Feel free to use books, Google or Stack Overflow, just like you would if you were programming normally, but please don't actively look for a solution to the problem as this will negatively affect the research.
+
+## Instructions
+
+Your task is to determine if all characters in a string are alphabetic, i.e., either an uppercase letter, or a lowercase letter.
+
+Your job is to define a function with the signature `str_isalpha`, which takes a string, and returns a non-zero value if all characters in the string are alphabetic and there is at least one character, zero otherwise.
+
+For example:
+
+```c
+str_isalpha("Hello");
+// Returns: non-zero
+```
