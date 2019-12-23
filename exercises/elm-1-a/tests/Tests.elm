8b4f3dace55d2ffa7e93ec15f686462d4fe465b5
CeddlyBurge <ceddlyburge@gmail.com>
2019-12-23 18:04:50 +0000

    Fix in to -> into typo

--- exercises/elm-1-a/tests/Tests.elm
+++ exercises/elm-1-a/tests/Tests.elm
@@ -24,7 +24,7 @@ tests =
             \() ->
                 aggregateScorers [ "Lisa", "Anna" ]
                 |> Expect.equal [ "Anna", "Lisa" ]  
-        , test "converts duplicates in to goals and maintain single scorers" <|
+        , test "converts duplicates into goals and maintain single scorers" <|
             \() ->
                 aggregateScorers [ "Lisa", "Anna", "Anna" ]
                 |> Expect.equal [ "Anna (2)", "Lisa" ]                    
