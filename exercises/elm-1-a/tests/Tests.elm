abc8c2c79c6beff481622c67abdc1691a9b344cd
CeddlyBurge <ceddlyburge@gmail.com>
2019-12-23 06:46:38 +0000

    Update test names

--- exercises/elm-1-a/tests/Tests.elm
+++ exercises/elm-1-a/tests/Tests.elm
@@ -8,15 +8,15 @@ import Test exposing (..)
 tests : Test
 tests =
     describe "AggregateScorers"
-        [ test "single scorer is not changed" <|
+        [ test "doesn't change a single scorer" <|
             \() ->
                 aggregateScorers [ "Anna" ]
                 |> Expect.equal [ "Anna" ]
-        , test "multiple unique scorers are unchanged" <|
+        , test "doesn't change multiple unique scorers" <|
             \() ->
                 aggregateScorers [ "Anna", "Lisa" ]
                 |> Expect.equal [ "Anna", "Lisa" ]  
-        , test "duplicate names score a goal for each duplicate" <|
+        , test "converts duplicates in to goals" <|
             \() ->
                 aggregateScorers [ "Anna", "Anna" ]
                 |> Expect.equal [ "Anna (2)" ]                    
