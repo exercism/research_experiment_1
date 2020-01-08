c79d6afbc2aa3076f65b5c9595af886feaf3dbcb
CeddlyBurge <ceddlyburge@gmail.com>
2020-01-08 18:30:20 +0000

    Autoformat examples for elm-2-a

--- exercises/elm-2-a/.meta/Example.Dict.elm
+++ exercises/elm-2-a/.meta/Example.Dict.elm
@@ -3,48 +3,61 @@ module AggregateScorers exposing (..)
 import Dict exposing (..)
 import Array exposing (..)
 
-type alias Player = {
-    name : String
+
+type alias Player =
+    { name : String
     , goals : List Int
     }
 
+
 aggregateScorers : List String -> List String
 aggregateScorers playerNames =
-    List.foldl  updatePlayer Dict.empty playerNames
-    |> Dict.values
-    |> List.map formatPlayer
+    List.foldl updatePlayer Dict.empty playerNames
+        |> Dict.values
+        |> List.map formatPlayer
+
 
 updatePlayer : String -> Dict String Player -> Dict String Player
-updatePlayer goal players  =
+updatePlayer goal players =
     let
-        (playerName, goalTime) = parseGoal goal
-        existingPlayer = Dict.get playerName players
-        newPlayer = case existingPlayer of 
-            Just player ->
-                { player | goals = List.append player.goals [ goalTime ]  }
-            Nothing ->
-                (Player playerName [ goalTime ])
+        ( playerName, goalTime ) =
+            parseGoal goal
+
+        existingPlayer =
+            Dict.get playerName players
+
+        newPlayer =
+            case existingPlayer of
+                Just player ->
+                    { player | goals = List.append player.goals [ goalTime ] }
+
+                Nothing ->
+                    (Player playerName [ goalTime ])
     in
         Dict.insert playerName newPlayer players
 
-parseGoal : String -> (String, Int)
+
+parseGoal : String -> ( String, Int )
 parseGoal goal =
     let
-        split = String.split " " goal |> Array.fromList
-        playerName =  
+        split =
+            String.split " " goal |> Array.fromList
+
+        playerName =
             Array.get 0 split
-            |> Maybe.withDefault ""
-        goalTime =  
+                |> Maybe.withDefault ""
+
+        goalTime =
             (Array.get 1 split)
-            |> Maybe.andThen String.toInt
-            |> Maybe.withDefault 0
+                |> Maybe.andThen String.toInt
+                |> Maybe.withDefault 0
     in
-        (playerName, goalTime)
+        ( playerName, goalTime )
+
 
 formatPlayer : Player -> String
 formatPlayer player =
-        player.name 
-        ++ " (" 
-        ++ String.join ", " (List.map String.fromInt player.goals) 
+    player.name
+        ++ " ("
+        ++ String.join ", " (List.map String.fromInt player.goals)
         ++ ")"
-
--- exercises/elm-2-a/.meta/Example.List.elm
+++ exercises/elm-2-a/.meta/Example.List.elm
@@ -3,53 +3,65 @@ module AggregateScorers exposing (..)
 import Dict exposing (..)
 import Array exposing (..)
 
-type alias Scorer = 
-    { playerName: String
-    , goals: List String
+
+type alias Scorer =
+    { playerName : String
+    , goals : List String
     }
+
 
 aggregateScorers : List String -> List String
 aggregateScorers goals =
     let
-        playerNameGoalTimes = List.map parseGoal goals
-        playerNames = List.map Tuple.first playerNameGoalTimes
+        playerNameGoalTimes =
+            List.map parseGoal goals
+
+        playerNames =
+            List.map Tuple.first playerNameGoalTimes
     in
         List.map (scorerWithGoalsFromDuplicates playerNames playerNameGoalTimes) playerNames
-        |> List.map formatScorer
-        |> List.foldr keepUnique []
-        |> List.sort
+            |> List.map formatScorer
+            |> List.foldr keepUnique []
+            |> List.sort
 
-parseGoal : String -> (String, String)
+
+parseGoal : String -> ( String, String )
 parseGoal goal =
     let
-        split = Array.fromList (String.split " " goal)
-        playerName = 
-            Array.get 0 split
-            |> Maybe.withDefault ""
-        goalTime = 
-            Array.get 1 split 
-            |> Maybe.withDefault ""
-    in
-        (playerName, goalTime)
+        split =
+            Array.fromList (String.split " " goal)
 
-scorerWithGoalsFromDuplicates : List String -> List (String, String) -> String -> Scorer
-scorerWithGoalsFromDuplicates playerNames playerNameGoalTimes playerName  =
+        playerName =
+            Array.get 0 split
+                |> Maybe.withDefault ""
+
+        goalTime =
+            Array.get 1 split
+                |> Maybe.withDefault ""
+    in
+        ( playerName, goalTime )
+
+
+scorerWithGoalsFromDuplicates : List String -> List ( String, String ) -> String -> Scorer
+scorerWithGoalsFromDuplicates playerNames playerNameGoalTimes playerName =
     let
-        goalTimes = 
-            List.filter (\(p, g) -> p == playerName) playerNameGoalTimes 
-            |> List.map Tuple.second
+        goalTimes =
+            List.filter (\( p, g ) -> p == playerName) playerNameGoalTimes
+                |> List.map Tuple.second
     in
         Scorer playerName goalTimes
 
+
 formatScorer : Scorer -> String
 formatScorer player =
-        player.playerName 
-        ++ " (" 
+    player.playerName
+        ++ " ("
         ++ String.join ", " player.goals
         ++ ")"
 
+
 keepUnique : String -> List String -> List String
-keepUnique x xs  =
+keepUnique x xs =
     if List.any (\s -> s == x) xs then
         xs
     else
