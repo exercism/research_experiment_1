52267c3c159813863c9f8a5f052c4cb6e10fe3e6
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2020-01-13 00:28:40 +0100

    x86-64-assembly: Use 32-bit pointers
    Use 32-bit pointers relative to the jump table to make it smaller.

--- exercises/x86-64-assembly-2-b/.meta/x86_64_assembly_2_b.start3.asm
+++ exercises/x86-64-assembly-2-b/.meta/x86_64_assembly_2_b.start3.asm
@@ -10,20 +10,21 @@ indigo: db "INDIGO", 0
 violet: db "VIOLET", 0
 
 jump_table:
-dq label_red - jump_table
-dq label_orange - jump_table
-dq label_yellow - jump_table
-dq label_green - jump_table
-dq label_blue - jump_table
-dq label_indigo - jump_table
-dq label_violet - jump_table
+dd label_red - jump_table
+dd label_orange - jump_table
+dd label_yellow - jump_table
+dd label_green - jump_table
+dd label_blue - jump_table
+dd label_indigo - jump_table
+dd label_violet - jump_table
 
 section .text
 global color_name
 color_name:
     mov edi, edi
-    lea rax, [rel jump_table]
-    add rax, [rax + rdi * 8]
+    lea rdx, [rel jump_table]
+    movsx rax, dword [rdx + rdi * 4]
+    add rax, rdx
     jmp rax
 label_red:
     lea rax, [rel red]
