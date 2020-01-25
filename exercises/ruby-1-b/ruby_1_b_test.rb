3bd41153c03977326dc4b6cdda79775fdc056f55
goalaleo <goalaleo@gmail.com>
2020-01-25 23:03:33 +0200

    Update tests to use assert_equal assertions

--- exercises/ruby-1-b/ruby_1_b_test.rb
+++ exercises/ruby-1-b/ruby_1_b_test.rb
@@ -3,30 +3,30 @@ require_relative 'ruby_1_b'
 
 class LibraryTest < Minitest::Test
   def test_first_book_imported
-    assert Library.books.map(&:isbn).include?("9780191604928")
+    assert_includes Library.books.map(&:isbn), "9780191604928"
   end
 
   def test_last_book_imported
-    assert Library.books.map(&:isbn).include?("9780451524935")
+    assert_includes Library.books.map(&:isbn), "9780451524935"
   end
 
   def test_books_size_matches_records_count
-    assert Library.books.size == File.foreach("books.txt").count - 1
+    assert_equal IO.foreach("books.txt").count - 1, Library.books.size
   end
 
   def test_book_has_title
-    assert Library.books.first.title == "The Adventures of Tom Sawyer"
+    assert_equal "The Adventures of Tom Sawyer", Library.books.first.title
   end
 
   def test_book_has_author
-    assert Library.books.first.author == "Mark Twain"
+    assert_equal "Mark Twain", Library.books.first.author
   end
 
   def test_book_has_isbn
-    assert Library.books.first.isbn == "9780191604928"
+    assert_equal "9780191604928", Library.books.first.isbn
   end
 
   def test_book_has_published
-    assert Library.books.first.published == "2007"
+    assert_equal "2007", Library.books.first.published
   end
 end
