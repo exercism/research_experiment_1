b0946d9b7d673018828f459a6bd79108eb2791ed
Erik Schierboom <erik_schierboom@hotmail.com>
2020-02-20 14:33:39 +0100

    [Elm] Add tests to elm-2-b

--- exercises/elm-2-b/tests/Tests.elm
+++ exercises/elm-2-b/tests/Tests.elm
@@ -20,14 +20,38 @@ tests =
             \() ->
                 retrieveMenu "formaggio (v), tomato sauce, emmental, 8€, regina, tomato sauce, ham, mushrooms, cantal, 11€"
                     |> Expect.equal [ formaggio, regina ]
+        , test "retrieve large number of pizzas" <|
+            \() ->
+                retrieveMenu "formaggio (v), tomato sauce, emmental, 8€, tonno, tomato sauce, tuna, 10€, regina, tomato sauce, ham, mushrooms, cantal, 11€, margherita (v), tomato sauce, mozzarella, 7€, caprese (v), tomato sauce, spinach, mozzarella, 8€, hawaii, tomato sauce, pineapple, ham, 9€"
+                    |> Expect.equal [ formaggio, tonno, regina, margherita, caprese, hawaii ]
         ]
 
 
 formaggio : Pizza
 formaggio =
-    Pizza "formaggio" True ["tomato sauce", "emmental"] 8
+    Pizza "formaggio" True [ "tomato sauce", "emmental" ] 8
 
 
 regina : Pizza
 regina =
-    Pizza "regina" False ["tomato sauce", "ham", "mushrooms", "cantal"] 11
+    Pizza "regina" False [ "tomato sauce", "ham", "mushrooms", "cantal" ] 11
+
+
+tonno : Pizza
+tonno =
+    Pizza "tonno" False [ "tomato sauce", "tuna" ] 10
+
+
+margherita : Pizza
+margherita =
+    Pizza "margherita" True [ "tomato sauce", "mozzarella" ] 7
+
+
+caprese : Pizza
+caprese =
+    Pizza "caprese" True [ "tomato sauce", "spinach", "mozzarella" ] 8
+
+
+hawaii : Pizza
+hawaii =
+    Pizza "hawaii" False [ "tomato sauce", "pineapple", "ham" ] 9
