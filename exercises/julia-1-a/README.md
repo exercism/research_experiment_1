37e2bcedec5ec50a6d336c89e542006c568663cb
Sascha Mann <git@mail.saschamann.eu>
2020-07-15 19:28:09 +0200

    Julia: Add ex1a tic-tac-toe

---
+++ exercises/julia-1-a/README.md
@@ -0,0 +1,98 @@
+## Instructions
+
+Implement your own [tic-tac-toe](https://en.wikipedia.org/wiki/Tic-tac-toe) game.
+(_You may also know the game as "noughts and crosses" or "Xs and Os"._)
+
+The games is played on a 3x3 grid.
+Players take turns to place `X`s and `O`s on the grid.
+The game ends when one player has won by placing three of marks in a row, column, or along a diagonal of the grid, or when the entire grid is filled up.
+
+Without loss of generality we will assume that `X` starts.
+
+At first, you will create create a function `gamestate` that takes the game board represented a string as its sole argument and returns a Symbol describing the state of the game.
+The board will be given as a 3-line string containing `X`s, `O`s and ` `s:
+
+```julia
+board = "X  \n XO\nOX "
+```
+
+represents the following board:
+
+```
+   |   |   
+ X |   |   
+___|___|___
+   |   |   
+   | X | O 
+___|___|___
+   |   |
+ O | X |  
+   |   |
+```
+
+There are 3 potential game states:
+
+- The game is **ongoing**.
+- The game ended in a **draw**.
+- The game ended in a **win**.
+
+If the given board is invalid, throw an appropriate error.
+
+### Examples
+
+#### Ongoing game
+
+```
+   |   |   
+ X |   |   
+___|___|___
+   |   |   
+   | X | O 
+___|___|___
+   |   |
+ O | X |  
+   |   |
+```
+
+```julia
+julia> gamestate("X  \n XO\nOX ")
+:ongoing
+```
+
+#### Draw
+
+```
+   |   |   
+ X | O | X 
+___|___|___
+   |   |   
+ X | X | O 
+___|___|___
+   |   |
+ O | X | O
+   |   |
+```
+
+```julia
+julia> gamestate("XOX\nXXO\nOXO")
+:draw
+```
+
+#### Win
+
+```
+   |   |   
+ X | X | X 
+___|___|___
+   |   |   
+   | O | O 
+___|___|___
+   |   |
+   |   |  
+   |   |
+```
+
+```julia
+julia> gamestate("XXX\n OO\n   ")
+:win
+```
---
+++ exercises/julia-1-a/runtests.jl
@@ -0,0 +1,214 @@
+using Test
+
+include("tictactoe.jl")
+
+@testset "Finished games where X won via colum victory" begin
+    boards = [
+        """
+        XOO
+        X  
+        X  
+        """,
+        """
+        OXO
+         X 
+         X 
+        """,
+        """
+        OOX
+          X
+          X
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :win
+    end
+end
+
+@testset "Finished games where O won via colum victory" begin
+    boards = [
+        """
+        OXX
+        OX 
+        O  
+        """,
+        """
+        XOX
+         OX
+         O 
+        """,
+        """
+        XXO
+         XO
+          O
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :win
+    end
+end
+
+@testset "Finished games where X won via row victory" begin
+    boards = [
+        """
+        XXX
+        XOO
+        O  
+        """,
+        """
+        O O
+        XXX
+         O 
+        """,
+        """
+         OO
+        O X
+        XXX
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :win
+    end
+end
+
+@testset "Finished games where O won via row victory" begin
+    boards = [
+        """
+        OOO
+        XXO
+        XX 
+        """,
+        """
+        XX 
+        OOO
+        X
+        """,
+        """
+        XOX
+         XX
+        OOO
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :win
+    end
+end
+
+@testset "Finished games where X won via diagonal victory" begin
+    boards = [
+        """
+        XOO
+         X 
+          X
+        """,
+        """
+        O X
+        OX 
+        X  
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :win
+    end
+end
+
+@testset "Finished games where O won via diagonal victory" begin
+    boards = [
+        """
+        OXX
+        OOX 
+        X O
+        """,
+        """
+          O
+         OX
+        OXX
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :win
+    end
+end
+
+@testset "Draws" begin
+    boards = [
+        """
+        XOX
+        XXO
+        OXO
+        """,
+        """
+        XXO
+        OOX
+        OXX
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :draw
+    end
+end
+
+@testset "Not-yet-finished games" begin
+    boards = [
+        """
+           
+         X  
+           
+        """,
+        """
+        O  
+         X
+           
+        """,
+        """
+        X  
+         XO
+        OX 
+        """
+    ]
+
+    for b in boards
+        @test gamestate(b) == :ongoing
+    end
+end
+
+@testset "Invalid boards" begin
+    boards = [
+        """
+        XX 
+           
+           
+        """,
+        """
+        OOX
+           
+           
+        """,
+        """
+        XXX
+        XOO
+        XOO
+        """,
+        """
+        XOX
+        OXO
+        XOX
+        """,
+        """
+        XXX
+        OOO
+           
+        """
+    ]
+
+    for b in boards
+        @test_throws Exception gamestate(b)
+    end
+end
