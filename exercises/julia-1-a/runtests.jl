c5b6ab1a225eed6d81af3c58cb2fa33115b4f305
Sascha Mann <git@mail.saschamann.eu>
2020-07-22 23:39:37 +0200

    Only have single-test testsets
    The let blocks should really be a testset, but the test-runner doesn't support nested testsets yet.
    
    This makes it easier for the student to debug the tests, even if it's not how you'd typically write tests

--- exercises/julia-1-a/runtests.jl
+++ exercises/julia-1-a/runtests.jl
@@ -2,188 +2,180 @@ using Test
 
 include("tictactoe.jl")
 
-@testset "Finished games where X won via colum victory" begin
-    boards = [
-        """
-        XOO
-        X  
-        X  """,
-        """
-        OXO
-         X 
-         X """,
-        """
-        OOX
-          X
-          X"""
-    ]
+let boards = [
+    """
+    XOO
+    X  
+    X  """,
+    """
+    OXO
+     X 
+     X """,
+    """
+    OOX
+      X
+      X"""
+]
+   
 
-    for b in boards
+    @testset "Finished game where X won via colum victory: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :win
     end
 end
 
-@testset "Finished games where O won via colum victory" begin
-    boards = [
-        """
-        OXX
-        OX 
-        O  """,
-        """
-        XOX
-         OX
-         O """,
-        """
-        XXO
-         XO
-          O"""
-    ]
+let boards = [
+    """
+    OXX
+    OX 
+    O  """,
+    """
+    XOX
+     OX
+     O """,
+    """
+    XXO
+     XO
+      O"""
+]
 
-    for b in boards
+    @testset "Finished game where O won via colum victory: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :win
     end
 end
 
-@testset "Finished games where X won via row victory" begin
-    boards = [
-        """
-        XXX
-        XOO
-        O  """,
-        """
-        O O
-        XXX
-         O """,
-        """
-         OO
-        O X
-        XXX"""
-    ]
+let boards = [
+    """
+    XXX
+    XOO
+    O  """,
+    """
+    O O
+    XXX
+     O """,
+    """
+     OO
+    O X
+    XXX"""
+]
 
-    for b in boards
+    @testset "Finished game where X won via row victory: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :win
     end
 end
 
-@testset "Finished games where O won via row victory" begin
-    boards = [
-        """
-        OOO
-        XXO
-        XX """,
-        """
-        XX 
-        OOO
-        X  """,
-        """
-        XOX
-         XX
-        OOO"""
-    ]
+let boards = [
+    """
+    OOO
+    XXO
+    XX """,
+    """
+    XX 
+    OOO
+    X  """,
+    """
+    XOX
+     XX
+    OOO"""
+]
 
-    for b in boards
+    @testset "Finished game where O won via row victory: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :win
     end
 end
 
-@testset "Finished games where X won via diagonal victory" begin
-    boards = [
-        """
-        XOO
-         X 
-          X""",
-        """
-        O X
-        OX 
-        X  """
-    ]
+let boards = [
+    """
+    XOO
+     X 
+      X""",
+    """
+    O X
+    OX 
+    X  """
+]
 
-    for b in boards
+    @testset "Finished game where X won via diagonal victory: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :win
     end
 end
 
-@testset "Finished games where O won via diagonal victory" begin
-    boards = [
-        """
-        OXX
-        OOX
-        X O""",
-        """
-          O
-         OX
-        OXX"""
-    ]
+let boards = [
+    """
+    OXX
+    OOX
+    X O""",
+    """
+      O
+     OX
+    OXX"""
+]
 
-    for b in boards
+    @testset "Finished game where O won via diagonal victory: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :win
     end
 end
 
-@testset "Draws" begin
-    boards = [
-        """
-        XOX
-        XXO
-        OXO""",
-        """
-        XXO
-        OXX
-        XOO"""
-    ]
+let boards = [
+    """
+    XOX
+    XXO
+    OXO""",
+    """
+    XXO
+    OXX
+    XOO"""
+]
 
-    for b in boards
+    @testset "Draw: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :draw
     end
 end
 
-@testset "Ongoing games" begin
-    # The first test is parsed strangely so it requires extra trailing spaces
-    boards = [
-        """
-            
-         X  
-            """,
-        """
-        O  
-         X 
-           """,
-        """
-        X  
-         XO
-        OX """
-    ]
+# The first test is parsed strangely so it requires extra trailing spaces
+let boards = [
+    """
+        
+     X  
+        """,
+    """
+    O  
+     X 
+       """,
+    """
+    X  
+     XO
+    OX """
+]
 
-    for b in boards
+    @testset "Ongoing game: \"$(escape_string(b))\"" for b in boards
         @test gamestate(b) == :ongoing
     end
 end
 
-@testset "Invalid boards" begin
-    boards = [
-        """
-        XX 
-           
-           """,
-        """
-        OOX
-           
-           """,
-        """
-        XXX
-        XOO
-        XOO""",
-        """
-        XOX
-        OXO
-        XOX""",
-        """
-        XXX
-        OOO
-           """
-    ]
+let boards = [
+    """
+    XX 
+       
+       """,
+    """
+    OOX
+       
+       """,
+    """
+    XXX
+    XOO
+    XOO""",
+    """
+    XOX
+    OXO
+    XOX""",
+    """
+    XXX
+    OOO
+       """
+]
 
-    for b in boards
+    @testset "Invalid board: \"$(escape_string(b))\"" for b in boards
         @test_throws Exception gamestate(b)
     end
 end
